193b78a6cd3e86b037ec7b2d5dde5c658c1936f5
Vasek Sraier <vasek@sraier.cz>
2017-01-30 21:51:18 +0100

    massive refactoring and optimalizations

--- app/src/main/java/com/mukeshsolanki/countrypickerexample/MainActivity.java
+++ app/src/main/java/com/mukeshsolanki/countrypickerexample/MainActivity.java
@@ -6,71 +6,77 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
-import com.mukesh.countrypicker.fragments.CountryPicker;
-import com.mukesh.countrypicker.interfaces.CountryPickerListener;
-import com.mukesh.countrypicker.models.Country;
+
+import com.mukesh.countrypicker.CountryPicker;
+import com.mukesh.countrypicker.CountryPickerListener;
+import com.mukesh.countrypicker.Country;
 
 import java.util.ArrayList;
 import java.util.Collections;
 
 public class MainActivity extends AppCompatActivity {
 
-  private TextView mCountryNameTextView, mCountryIsoCodeTextView, mCountryDialCodeTextView;
-  private ImageView mCountryFlagImageView;
-  private Button mPickCountryButton;
-  private CountryPicker mCountryPicker;
+    private TextView mCountryNameTextView, mCountryIsoCodeTextView, mCountryDialCodeTextView;
+    private ImageView mCountryFlagImageView;
+    private Button mPickCountryButton;
+    private CountryPicker mCountryPicker;
 
-  @Override protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
-    initialize();
-    setListener();
-  }
-
-  private void setListener() {
-    mCountryPicker.setListener(new CountryPickerListener() {
-      @Override public void onSelectCountry(String name, String code, String dialCode,
-          int flagDrawableResID) {
-        mCountryNameTextView.setText(name);
-        mCountryIsoCodeTextView.setText(code);
-        mCountryDialCodeTextView.setText(dialCode);
-        mCountryFlagImageView.setImageResource(flagDrawableResID);
-        mCountryPicker.dismiss();
-      }
-    });
-    mPickCountryButton.setOnClickListener(new View.OnClickListener() {
-      @Override public void onClick(View v) {
-        mCountryPicker.show(getSupportFragmentManager(), "COUNTRY_PICKER");
-      }
-    });
-    getUserCountryInfo();
-  }
-
-  private void initialize() {
-    mCountryNameTextView = (TextView) findViewById(R.id.selected_country_name_text_view);
-    mCountryIsoCodeTextView = (TextView) findViewById(R.id.selected_country_iso_text_view);
-    mCountryDialCodeTextView = (TextView) findViewById(R.id.selected_country_dial_code_text_view);
-    mPickCountryButton = (Button) findViewById(R.id.country_picker_button);
-    mCountryFlagImageView = (ImageView) findViewById(R.id.selected_country_flag_image_view);
-    mCountryPicker = CountryPicker.newInstance("Select Country");
-
-    // You can limit the displayed countries
-    ArrayList<Country> nc = new ArrayList<>();
-    for (Country c : Country.getAllCountries()) {
-      if (c.getDialCode().endsWith("0")) {
-        nc.add(c);
-      }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initialize();
+        setListener();
     }
-    // and decide, in which order they will be displayed
-    Collections.reverse(nc);
-    mCountryPicker.setCountriesList(nc);
-  }
 
-  private void getUserCountryInfo() {
-    Country country = mCountryPicker.getUserCountryInfo(this);
-    mCountryFlagImageView.setImageResource(country.getFlag());
-    mCountryDialCodeTextView.setText(country.getDialCode());
-    mCountryIsoCodeTextView.setText(country.getCode());
-    mCountryNameTextView.setText(country.getName());
-  }
+    private void setListener() {
+        mCountryPicker.setListener(new CountryPickerListener() {
+            @Override
+            public void onSelectCountry(String name, String code, String dialCode,
+                                        int flagDrawableResID) {
+                mCountryNameTextView.setText(name);
+                mCountryIsoCodeTextView.setText(code);
+                mCountryDialCodeTextView.setText(dialCode);
+                mCountryFlagImageView.setImageResource(flagDrawableResID);
+                mCountryPicker.dismiss();
+            }
+        });
+        mPickCountryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCountryPicker.show(getSupportFragmentManager(), "COUNTRY_PICKER");
+            }
+        });
+        getUserCountryInfo();
+    }
+
+    private void initialize() {
+        mCountryNameTextView = (TextView) findViewById(R.id.selected_country_name_text_view);
+        mCountryIsoCodeTextView = (TextView) findViewById(R.id.selected_country_iso_text_view);
+        mCountryDialCodeTextView = (TextView) findViewById(R.id.selected_country_dial_code_text_view);
+        mPickCountryButton = (Button) findViewById(R.id.country_picker_button);
+        mCountryFlagImageView = (ImageView) findViewById(R.id.selected_country_flag_image_view);
+        mCountryPicker = CountryPicker.newInstance("Select Country");
+
+        // You can limit the displayed countries
+        ArrayList<Country> nc = new ArrayList<>();
+        for (Country c : Country.getAllCountries()) {
+            if (c.getDialCode().endsWith("0")) {
+                nc.add(c);
+            }
+        }
+        // and decide, in which order they will be displayed
+        Collections.reverse(nc);
+        mCountryPicker.setCountriesList(nc);
+    }
+
+    private void getUserCountryInfo() {
+        Country country = Country.getCountryFromSIM(getApplicationContext());
+        if (country != null) {
+            mCountryFlagImageView.setImageResource(country.getFlag());
+            mCountryDialCodeTextView.setText(country.getDialCode());
+            mCountryIsoCodeTextView.setText(country.getCode());
+            mCountryNameTextView.setText(country.getName());
+        }
+    }
 }
---
+++ countrypicker/src/main/java/com/mukesh/countrypicker/CountryPickerListener.java
@@ -0,0 +1,8 @@
+package com.mukesh.countrypicker;
+
+/**
+ * Created by mukesh on 25/04/16.
+ */
+public interface CountryPickerListener {
+    public void onSelectCountry(String name, String code, String dialCode, int flagDrawableResID);
+}
